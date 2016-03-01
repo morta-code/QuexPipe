dc94fec125c1f263fdfba5b8a803587c7c48deaa
Móréh Tamás <morta@digitus.itk.ppke.hu>
2016-03-01 17:09:04 +0100

    Documentations included.

--- dev-tool/include/quexpipe/implementation_point_queue.h
+++ dev-tool/include/quexpipe/implementation_point_queue.h
@@ -5,7 +5,7 @@
 #include <quexpipe/quexpipeitem.h>
 
 /**************************************************************
- *   The hidden part of the dynamic library
+ *   The implementation part of the dynamic library
  **************************************************************/
 
 
@@ -15,56 +15,68 @@
  */
 namespace quexpipeitem {
 
-QUEX_TYPE_STRING        (*read_from_source)(bool&, bool&) = 0;  ///< The function pointer which reads the input text
-QUEX_TYPE_ANALYZER*     lexer;                                  ///< Pointer for the Quex lexer object
+QUEX_TYPE_STRING     (*read_from_source)(bool&, bool&) = 0;    ///< The function pointer which reads the input text
+QUEX_TYPE_ANALYZER*  lexer;                                    ///< Pointer for the Quex lexer object
+bool                 source_terminated;                        ///< True if input source is terminated
+bool                 ok;                                       ///< Indicates errors while analysis. (Not used.)
 
-bool                    source_terminated;                      ///< True if input source is terminated
-bool                    ok;
+QUEX_TYPE_CHARACTER  QX_BUFFER[QUEX_SETTING_BUFFER_SIZE + 10]; ///< The local buffer. It will be used by the lexer.
+QUEX_TYPE_STRING     fill_back;                                ///< Stores chars over the buffer size.
+QUEX_TYPE_STRING     input_buffer;                             ///< Builds up the buffer from remainders and the input.
 
-QUEX_TYPE_CHARACTER     QX_BUFFER[QUEX_SETTING_BUFFER_SIZE + 10];  ///< The local buffer. It will be sign by the chunk.
-
-QUEX_TYPE_STRING        fill_back;
-QUEX_TYPE_STRING        input_buffer;
-
-QUEX_TYPE_STRING        prev_token_text;
-
-QUEX_TYPE_TOKEN*        tokens_begin = 0;
-QUEX_TYPE_TOKEN*        tokens_end = 0;
-QUEX_TYPE_TOKEN*        tokens_iterator = 0;
+QUEX_TYPE_STRING     prev_token_text;                          ///< Stores the text of the previous received token.
+QUEX_TYPE_TOKEN*     tokens_begin = 0;                         ///< First element of the received token queue.
+QUEX_TYPE_TOKEN*     tokens_end = 0;                           ///< Exclusive upper boundary of the token queue.
+QUEX_TYPE_TOKEN*     tokens_iterator = 0;                      ///< Pointer to the current token.
 
 
+/**
+ * @brief Initializes the library.
+ * Creates the lexer.
+ */
 inline void initialize(unsigned int &)
 {
     source_terminated = false;
     ok = true;
     lexer = new QUEX_TYPE_ANALYZER((QUEX_TYPE_CHARACTER*)0, 0, (QUEX_TYPE_CHARACTER*)0);
-    input_buffer.reserve(QUEX_SETTING_BUFFER_SIZE);
 }
 
+
+/**
+ * @brief Closes the library.
+ * Destructs the lexer.
+ */
 inline void finalize()
 {
     delete lexer;
 }
 
 
-// Ez jó!
+/**
+ * @brief Fills the local buffer from remainders and the input.
+ * After the filling it moves data from input_buffer to QX_BUFFER.
+ * It also fills fill_back when more caracters arrived then the limit.
+ */
 inline void fill()
 {
+    // Sets the first (protected position) char to 0x0 (by the pointing method constraint).
     QX_BUFFER[0] = (QUEX_TYPE_CHARACTER)0;
+    // Loads back the previous input-overflowed data, then empty it.
     input_buffer += fill_back;
     fill_back.clear();
-
+    // Loads data from input as many as possible.
     while (input_buffer.size() < QUEX_SETTING_BUFFER_SIZE && !source_terminated) {
         input_buffer += read_from_source(source_terminated, ok);
     }
-
+    // When more data is loaded than the buffer limit,
+    // it should be cut and the remainder will be stored away for the next filling cycle.
     size_t qx_size = input_buffer.size();
     if (QUEX_SETTING_BUFFER_SIZE < qx_size) {
         qx_size = QUEX_SETTING_BUFFER_SIZE;
         fill_back += input_buffer.substr(qx_size - 1);
         input_buffer.resize(qx_size);
     }
-
+    // Copies data into the buffer and calls the lexer.
     memcpy(QX_BUFFER + 1, input_buffer.c_str(),
            sizeof(QUEX_TYPE_CHARACTER)*(qx_size+1));
     lexer->reset_buffer(QX_BUFFER, qx_size+1, QX_BUFFER+qx_size);
@@ -72,35 +84,46 @@ inline void fill()
 }
 
 
+/**
+ * @brief Process the input token by token.
+ * @param finished bool reference will be true when the last token is produced.
+ * @return the text content of the arrived token.
+ */
 inline QUEX_TYPE_STRING process(bool &finished, bool &)
 {
-    QUEX_TYPE_STRING result;
-    while (true) {
-        if (tokens_iterator < tokens_end) {
-            if (tokens_iterator->type_id() == QUEX_TKN_TERMINATION) {
-                // Ha már kifogyott a forrás, előző kiírása.
+    // Cycle until a valid token arrived.
+    do {
+        if (tokens_iterator < tokens_end) {  // There are tokens in the queue.
+            if (tokens_iterator->type_id() == QUEX_TKN_TERMINATION) {  // Term.token: The buffer became empty.
+                // If the input of the current lexer is empty, too, it sets finished and returns the remaining text.
                 if (source_terminated) {
                     finished = true;
                     return prev_token_text;
                 }
-                // Előző text visszatöltése (unused/LeMonde), feltöltés és újraolvasás kérése
+                // If the buffer can be refilled, loads back first the unconfirmed token text
+                // to avoid the so-called LeMonde problem (see the Quex documentation).
                 input_buffer += prev_token_text;
                 prev_token_text.clear();
                 fill();
+                // Empties the queue. (See down.)
                 tokens_iterator = tokens_end;
             } else {
-                // Előző kiírása, jelenlegi az előzőbe és léptetés
-                result += prev_token_text;
+                // A valid token arrived, it confirms the previous is also valid.
+                // Stores the current token's text away and returns the previous'.
+                QUEX_TYPE_STRING result = prev_token_text;
                 prev_token_text.clear();
                 prev_token_text += tokens_iterator->text;
+                // Step to the next token.
                 tokens_iterator++;
                 return result;
             }
-        } else {
+        } else {  // The token queue is empty.
+            // Performs the analysis and sets the corresponding token pointers.
+            // (By the lexer-managed memory management.)
             lexer->receive(&tokens_iterator);
             lexer->token_queue_remainder_get(&tokens_begin, &tokens_end);
         }
-    }
+    } while (true);
 
 }
 
@@ -109,7 +132,7 @@ inline QUEX_TYPE_STRING process(bool &finished, bool &)
 
 
 /**************************************************************
- *   Mandatory (public) functions definitions
+ *   Mandatory (public) function definitions
  **************************************************************/
 
 
--- dev-tool/include/quexpipe/implementation_point_single.h
+++ dev-tool/include/quexpipe/implementation_point_single.h
@@ -5,7 +5,7 @@
 #include <quexpipe/quexpipeitem.h>
 
 /**************************************************************
- *   The hidden part of the dynamic library
+ *   The implementation part of the dynamic library
  **************************************************************/
 
 
@@ -15,60 +15,72 @@
  */
 namespace quexpipeitem {
 
+QUEX_TYPE_STRING     (*read_from_source)(bool&, bool&) = 0;     ///< The function pointer which reads the input text
+QUEX_TYPE_ANALYZER*  lexer;                                     ///< Pointer for the Quex lexer object
 
-QUEX_TYPE_STRING        (*read_from_source)(bool&, bool&) = 0;  ///< The function pointer which reads the input text
-QUEX_TYPE_ANALYZER*     lexer;                                  ///< Pointer for the Quex lexer object
+QUEX_TYPE_CHARACTER  QX_BUFFER[QUEX_SETTING_BUFFER_SIZE + 10];  ///< The local buffer. It will be sign by the chunk.
+unsigned int         buffer_size;                               ///< Minimum buffer size. Set by the user.
+bool                 source_terminated;                         ///< True if input source is terminated
+bool                 ok;                                        ///< Indicates errors while analysis. (Not used.)
 
-QUEX_TYPE_CHARACTER     QX_BUFFER[QUEX_SETTING_BUFFER_SIZE + 10];  ///< The local buffer. It will be sign by the chunk.
+QUEX_TYPE_TOKEN      token_bank[2];                             ///< Stores the current and the previous token.
+QUEX_TYPE_TOKEN*     prev_token;                                ///< Pointer to the previous token.
+QUEX_TYPE_TOKEN_ID   token_id = QUEX_TKN_TERMINATION;           ///< The type-id of the current token.
 
-unsigned int            buffer_size;                            ///< Minimum buffer size. Set by the user.
-bool                    source_terminated;                      ///< True if input source is terminated
-bool                    ok;
-
-
-QUEX_TYPE_TOKEN         token_bank[2];
-QUEX_TYPE_TOKEN*        prev_token;
-QUEX_TYPE_TOKEN_ID      token_id = QUEX_TKN_TERMINATION;
-
-QUEX_TYPE_STRING        fill_back;
-QUEX_TYPE_STRING        input_buffer;
+QUEX_TYPE_STRING     fill_back;                                ///< Stores chars over the buffer size.
+QUEX_TYPE_STRING     input_buffer;                             ///< Builds up the buffer from remainders and the input.
 
 
+/**
+ * @brief Initializes the library.
+ * Creates the lexer and initializes token_bank.
+ */
 inline void initialize(unsigned int &)
 {
     source_terminated = false;
     ok = true;
     lexer = new QUEX_TYPE_ANALYZER((QUEX_TYPE_CHARACTER*)0, 0, (QUEX_TYPE_CHARACTER*)0);
-    input_buffer.reserve(QUEX_SETTING_BUFFER_SIZE);
     prev_token = &(token_bank[1]);
     token_bank[0].set(QUEX_TKN_TERMINATION);
     lexer->token_p_switch(&token_bank[0]);
 }
 
+
+/**
+ * @brief Closes the library.
+ * Destructs the lexer.
+ */
 inline void finalize()
 {
     delete lexer;
 }
 
 
-
+/**
+ * @brief Fills the local buffer from remainders and the input.
+ * After the filling it moves data from input_buffer to QX_BUFFER.
+ * It also fills fill_back when more caracters arrived then the limit.
+ */
 inline void fill()
 {
+    // Sets the first (protected position) char to 0x0 (by the pointing method constraint).
     QX_BUFFER[0] = (QUEX_TYPE_CHARACTER)0;
+    // Loads back the previous input-overflowed data, then empty it.
     input_buffer += fill_back;
     fill_back.clear();
-
+    // Loads data from input as many as possible.
     while (input_buffer.size() < QUEX_SETTING_BUFFER_SIZE && !source_terminated) {
         input_buffer += read_from_source(source_terminated, ok);
     }
-
+    // When more data is loaded than the buffer limit,
+    // it should be cut and the remainder will be stored away for the next filling cycle.
     size_t qx_size = input_buffer.size();
     if (QUEX_SETTING_BUFFER_SIZE < qx_size) {
         qx_size = QUEX_SETTING_BUFFER_SIZE;
         fill_back += input_buffer.substr(qx_size - 1);
         input_buffer.resize(qx_size);
     }
-
+    // Copies data into the buffer and calls the lexer.
     memcpy(QX_BUFFER + 1, input_buffer.c_str(),
            sizeof(QUEX_TYPE_CHARACTER)*(qx_size+1));
     lexer->reset_buffer(QX_BUFFER, qx_size+1, QX_BUFFER+qx_size);
@@ -76,48 +88,70 @@ inline void fill()
 }
 
 
+/**
+ * @brief Process the input token by token.
+ * @param finished bool reference will be true when the last token is produced.
+ * @return the text content of the arrived token.
+ */
 inline QUEX_TYPE_STRING process(bool &finished, bool &)
 {
+// Using in-code accumulator to avoid the so-called LeMonde problem.
+// (No accululator and multiple token-passing used in the Quex grammar! See the Quex doc.)
 #ifdef ACCUMULATOR_IN_IMPLEMENTATION
     QUEX_TYPE_STRING accumulator;
     accumulator.reserve(256);
     prev_token = lexer->token_p_switch(prev_token);
-    while (true) {
+    // Cycle until a valid token arrived.
+    do {
+        // Performs the analysis
         token_id = lexer->receive();
-        if (token_id == QUEX_TKN_TERMINATION) {
+        if (token_id == QUEX_TKN_TERMINATION) {  // Term.token: The buffer became empty.
             if (!source_terminated) {
+                // If the buffer can be refilled, loads back first the unconfirmed token text
+                // and the accumulator's content to avoid the so-called LeMonde problem (see the Quex documentation).
                 input_buffer += prev_token->text + accumulator;
                 fill();
                 accumulator.clear();
                 prev_token->text.clear();
             } else {
+                // If the input of the current lexer is empty, too, it sets finished and returns the remaining text.
                 finished = true;
                 return prev_token->text + accumulator;
             }
         } else if (token_id == QUEX_TKN_UNUSED) {
-                accumulator += lexer->token_p()->text;
+            // Push unused token text (actually character) to the accumulator.
+            accumulator += lexer->token_p()->text;
         } else {  // token_id is valid
+            // It validates the previous token and the accumulator content, therefore returns them.
             return prev_token->text + accumulator;
         }
 
-    }
+    } while (true);
+
+// Not using any accumulator in-code, however multiple token passing is also forbidden.
 #else
     prev_token = lexer->token_p_switch(prev_token);
-    while (true) {
+    // Cycle until a valid token arrived.
+    do {
+        // Performs the analysis
         token_id = lexer->receive();
-        if (token_id == QUEX_TKN_TERMINATION) {
+        if (token_id == QUEX_TKN_TERMINATION) {  // Term.token: The buffer became empty.
+            // If the input of the current lexer is empty, too, it sets finished and returns the remaining text.
             if (!source_terminated) {
+                // If the buffer can be refilled, loads back first the unconfirmed token text.
                 input_buffer += prev_token->text;
                 fill();
                 prev_token->text.clear();
             } else {
+                // If the input of the current lexer is empty, too, it sets finished and returns the remaining text.
                 finished = true;
                 return prev_token->text;
             }
-        } else  {
+        } else  {  // token_id is valid
+             // It validates the previous token, therefore returns them.
              return prev_token->text;
         }
-    }
+    } while (true);
 #endif
 }
 
