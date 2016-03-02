7cf65cf4ace99c1eb940d0a1198bfc95cbdc3093
Móréh Tamás <morta@digitus.itk.ppke.hu>
2016-03-02 16:30:40 +0100

    Install and uninstall script for dev-tools.

--- dev-tool/install.sh
+++ dev-tool/install.sh
@@ -1,5 +1,24 @@
 #!/bin/bash
 
-# root jog? Ha nem, akkor user space
-# target? [/usr]  /  [~.local] -> append to path?
-# $QUEX_PIPE? warning, ha nincs
+
+if [[ `whoami` != "root" ]]; then
+	echo -e "\e[91mRoot permission needed!\e[39m"
+	exit 1
+	# todo: userspace
+fi
+
+which quexpipe > /dev/null
+if [ $? -ne 0 ]; then
+	echo -e "\e[93mWARNING: There is no quexpipe installed!\e[39m" >&2
+fi
+
+TARGET="/usr"
+printf "\e[36mGive the installation target (where are /bin and /include subfolders)\e[39m\n[$TARGET] "
+read target
+if [[ $target == "" ]]; then
+	target=$TARGET
+fi
+
+install -o root -m 555 bin/quexpipe-plugin-build $target/bin/
+cp -R include/ $target
+printf "\e[36mSuccessfully installed!\n\e[39m"
--- dev-tool/uninstall.sh
+++ dev-tool/uninstall.sh
@@ -1,5 +1,20 @@
 #!/bin/bash
 
-# Hol van?
-# root jog?
-# törlés
+if [[ `whoami` != "root" ]]; then
+	echo -e "\e[91mRoot permission needed!\e[39m"
+	exit 1
+	# todo: userspace
+fi
+
+p=`which quexpipe-plugin-build`
+if [ $? -ne 0 ]; then
+	echo -e "\e[93mThere is no quexpipe-plugin-build installed!\e[39m" >&2
+else
+	rm $p
+	sudo rm -R `echo $p | sed -r "s=/bin/quexpipe-plugin-build=="`/include/quexpipe
+	if [ $? -eq 0 ]; then
+		printf "\e[36mSuccessfully uninstalled!\n\e[39m"
+	else
+		printf "\e[93mSomething went wrong. Check the files manually!\n\e[39m"
+	fi
+fi
