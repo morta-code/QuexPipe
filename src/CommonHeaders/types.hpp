6d7300051e25aa9ccaec3a2d924496333a13aeda
Móréh Tamás <morta@digitus.itk.ppke.hu>
2016-12-19 22:30:00 +0100

    Conversions between String8, String16 and String32
    This is an important step to the multiplatfom character representation.

--- src/CommonHeaders/types.hpp
+++ src/CommonHeaders/types.hpp
@@ -5,8 +5,9 @@
 
 #include	<vector>
 #include    <string>
+#include	<locale>
+#include	<codecvt>
 #include	<unordered_map>
-
 
 // --- type predeclarations --------------------------------------------------------------------------------------------
 
@@ -23,7 +24,49 @@ using String8			= std::basic_string<char>;
 using String16			= std::basic_string<char16_t>;
 using String32			= std::basic_string<char32_t>;
 
+inline
+String8 To_UTF8 (const String16& s)
+{
+    std::wstring_convert<std::codecvt_utf8_utf16<char16_t>, char16_t> conv;
+    return conv.to_bytes(s);
+}
 
+inline
+String8 To_UTF8 (const String32& s)
+{
+    std::wstring_convert<std::codecvt_utf8<char32_t>, char32_t> conv;
+    return conv.to_bytes(s);
+}
+
+inline
+String16 To_UTF16 (const String8& s)
+{
+    std::wstring_convert<std::codecvt_utf8_utf16<char16_t>, char16_t> conv;
+    return conv.from_bytes(s);
+}
+
+inline
+String16 To_UTF16 (const String32& s)
+{
+    std::wstring_convert<std::codecvt_utf16<char32_t>, char32_t> conv;
+    String8 bytes = conv.to_bytes(s);
+    return String16(reinterpret_cast<const char16_t*>(bytes.c_str()), bytes.length()/sizeof(char16_t));
+}
+
+inline
+String32 To_UTF32 (const String8& s)
+{
+    std::wstring_convert<std::codecvt_utf8<char32_t>, char32_t> conv;
+    return conv.from_bytes(s);
+}
+
+inline
+String32 To_UTF32 (const String16& s)
+{
+    const char16_t *pData = s.c_str();
+    std::wstring_convert<std::codecvt_utf16<char32_t>, char32_t> conv;
+    return conv.from_bytes(reinterpret_cast<const char*>(pData), reinterpret_cast<const char*>(pData+s.length()));
+}
 
 using LexerFactoryMap	= std::unordered_map<String8, ILexer* (*)()>;
 
